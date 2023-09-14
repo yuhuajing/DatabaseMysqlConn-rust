54f493f6521ef9f3dbd958275bc6cdc7882daf5f
yuhuajing <18852897525@163.com>
2023-09-14 12:00:02 +0800

    update

--- mysqlconn/src/main.rs
+++ mysqlconn/src/main.rs
@@ -12,22 +12,24 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let mysql_url: &str = "mysql://root:123456@localhost:3306/testUser";
     let pool = Pool::new(mysql_url)?;
     let mut conn = pool.get_conn()?;
-//     conn.exec_drop(
-//         r"INSERT INTO payment (customer_id, amount, account_name)
-// VALUES (:customer_id, :amount, :account_name)",
-//             params! {
-//                 "customer_id" => 2,
-//                         "amount" => 99,
-//                         "account_name" => "yhj",
-//             }
-//     )?;
-    let val: Option<String> =
-        conn.query_first("SELECT account_name from payment where customer_id=9")?;
-    let account_name:String = val.clone().unwrap_or_default();
-    
-    if "bar" == account_name{
-        println!("account_name = {account_name}");
+
+   // 预编译插入的执行语句
+   let query_state :&str = r"INSERT INTO payment (customer_id, amount, account_name) VALUES (:customer_id, :amount, :account_name)";
+    let insertstmt = conn.prep(
+        query_state,
+    )?;
+    for i in 10..15 {
+        conn.exec_drop(
+            &insertstmt,
+            params! {
+                "customer_id" => i,
+                        "amount" => 99,
+                        "account_name" => "clay",
+            },
+        )?;
     }
+
+
 
     Ok(())
 }
